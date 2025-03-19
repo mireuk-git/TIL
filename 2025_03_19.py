67e69c211eeb97da064ad5b0670652c275f11769
mireuk-git <alexyunwoo@gmail.com>
2025-03-19 17:25:42 +0900

    2025.03.19

---
+++ 2025_03_19.py
@@ -0,0 +1,40 @@
+# https://www.acmicpc.net/problem/21895
+# 21895
+'''
+n=int(input())
+A=input()
+B=input()
+StoI={"S":0, "R":1, "P":2}
+ItoS={0:"S", 1:"R", 2:"P"}
+
+for i in range(n):
+    a,b=StoI[A[i]],StoI[B[i]]
+    if a==b:
+        print((ItoS[(a+1)%3]),end='')
+    else: 
+        r=max(a,b)
+        if max(a,b)==2 and min(a,b)==0: r=0
+        print((ItoS[r]),end='')
+'''
+# https://www.acmicpc.net/problem/12645
+# 12645
+from itertools import permutations
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    l=[]
+    while n>0:
+        l.insert(0,n%10)
+        n//=10
+    n=int(''.join(map(str,l)))
+    l.sort()
+    perm=list(permutations(l))
+    r=0
+    for i in perm:
+        i=int(''.join(map(str,i)))
+        if i > n: 
+            r=i
+            break
+
+    print(f"Case #{_}:",r)
