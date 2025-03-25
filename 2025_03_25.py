5e822411dfa995673004a5bfa716474c48bd896b
mireuk-git <alexyunwoo@gmail.com>
2025-03-25 17:27:55 +0900

    2025.03.25

--- 2025_03_25.py
+++ 2025_03_25.py
@@ -1,3 +1,14 @@
 # https://www.acmicpc.net/problem/8989
 # 8989
+t=int(input())
+for _ in range(t):
+    l=list(input().split())
+    k=[[]]*len(l)
+    for i in range(len(l)):
+        k[i]=list(map(int,l[i].split(":")))
+        k[i][0]=(k[i][0]%12+k[i][1]/60)*5
+        k.append(abs(k[i][0]-k[i][1]))
+    z=sorted(zip(l,k))
+    l,k=zip(*z)
+    print(l[len(l)//2])
 
