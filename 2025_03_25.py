aa980e5e277e68187ac06dc9b9875c06bb1aeb0d
mireuk-git <alexyunwoo@gmail.com>
2025-03-26 16:27:47 +0900

    2025.03.25

--- 2025_03_25.py
+++ 2025_03_25.py
@@ -3,13 +3,13 @@
 t=int(input())
 for _ in range(t):
     l=list(input().split())
-    k=[[]]*len(l)
+    k=[]
     for i in range(len(l)):
-        k[i]=list(map(int,l[i].split(":")))
+        k.append(list(map(int,l[i].split(":"))))
         k[i][0]=(k[i][0]%12)*5+k[i][1]/12
         k[i]=(abs(k[i][0]-k[i][1]))
         k[i]=min(k[i],60-k[i])
-    z=sorted(zip(k,l),key=lambda x:(x[0],int(x[1][0:2])))
+    z=sorted(zip(k,l),key=lambda x:(x[0],int(x[1][0:2])*60+int(x[1][3:])))
     k,l=zip(*z)
     print(l[len(l)//2])
 
