61ba5780a70f7070d5b310c01ea5a6a7bdd4fcca
mireuk-git <alexyunwoo@gmail.com>
2025-07-17 18:48:33 +0900

    2025.07.17

---
+++ 2025_07_17.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/10469
+# 10469
+
+def err_validator(x,y):
+    for a,b in dir:
+        tx,ty=x,y
+        for i in range(7):
+            tx+=a
+            ty+=b
+            if tx>7 or tx<0 or ty>7 or ty<0: break
+            if board[tx][ty]=='*': 
+                return True
+
+board=[list(input()) for _ in range(8)]
+valid=True
+dir=[(1,0),(0,1),(1,1),(1,-1),(-1,0),(0,-1),(-1,-1),(-1,1)]
+queen_cnt=0
+
+for i in range(8):
+    for j in range(8):
+        if board[i][j]=='*':
+            queen_cnt+=1
+            if err_validator(i,j):
+                valid=False
+if queen_cnt!=8: 
+    vaild=False
+
+if valid: print('valid')
+else: print('invalid')
