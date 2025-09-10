f887c8131a240fd8f367d6e7f4359a9a4e74e5a3
mireuk-git <alexyunwoo@gmail.com>
2025-09-10 10:26:17 +0900

    p4635 2025.09.10

---
+++ java/codekata/src/p4635.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class p4635 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        while (n!=-1) {
+            int prevT=0;
+            int distance=0;
+            for (int i=0;i<n;i++){
+                int s = scanner.nextInt();
+                int t = scanner.nextInt();
+                distance=distance+s*(t-prevT);
+                prevT=t;
+            }
+            System.out.println(distance+" miles");
+            n=scanner.nextInt();
+        }
+        scanner.close();
+    }
+}
