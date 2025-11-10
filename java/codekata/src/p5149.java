dca1ab69666e0bed8827000c999d017952d867f4
mireuk-git <alexyunwoo@gmail.com>
2025-11-10 12:07:16 +0900

    p5149 2025.11.10

--- java/codekata/src/p5149.java
+++ java/codekata/src/p5149.java
@@ -1,32 +1,4 @@
-# Codekata
-### 5149
-[link](https://www.acmicpc.net/problem/5149)
-#### python
-```python
-k = int(input())
-for data_set in range(1,1+k):
-    n,m = map(int,input().split())
-    coords=[]
-    for i in range(n): coords.append(tuple(map(int,input().split())))
-    order = list(map(int,input().split()))
-    max_x,max_y,min_x,min_y = '','','',''
-    for i in order:
-        if max_x == '' or max_x < coords[i-1][0]: max_x = coords[i-1][0]
-        if min_x == '' or min_x > coords[i-1][0]: min_x = coords[i-1][0]
-        if max_y == '' or max_y < coords[i-1][1]: max_y = coords[i-1][1]
-        if min_y == '' or min_y > coords[i-1][1]: min_y = coords[i-1][1]
-    cnt=0
-    for i in coords:
-        if min_x <= i[0] <= max_x and min_y <= i[1] <= max_y: cnt+=1
-    print(f"Data Set {data_set}:")
-    print(cnt)
-    print()
-```
-- `coords` - x,y좌표를 튜플 형태로 묶어서 저장하는 리스트
-- `max_x`,`max_y`,`min_x`,`min_y` - 최소의 직사각형을 이루는 변의 좌표, `coords`를 순회하며 `cnt`를 올릴때의 기준선이 됨
 
-#### java
-```java
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -64,8 +36,7 @@ public class p5149 {
                 if (minX<=coords[i][0] && coords[i][0]<=maxX && minY<=coords[i][1] && coords[i][1]<=maxY) cnt++;
             }
             System.out.printf("Data Set %d:\n%d\n\n",dataSet,cnt);
+
         }
     }
 }
-```
-
