3375717a707acfbc756380a8208ac727bb24a9aa
mireuk-git <alexyunwoo@gmail.com>
2025-11-12 09:06:31 +0900

    2025.11.11

--- java/codekata/src/p28432.java
+++ java/codekata/src/p28432.java
@@ -1,28 +1,3 @@
-# Codekata
-### 28432
-[link](https://www.acmicpc.net/problem/28432)
-#### python
-```python
-n=int(input())
-s=[]
-for _ in range(n):
-    string=input()
-    if string=="?":
-        i=_
-    s.append(string)
-first,end='',''
-if i>0: first = s[i-1][-1]
-if i<n-1: end = s[i+1][0]
-
-m=int(input())
-for i in range(m):
-    a=input()
-    if first in a[0] and end in a[-1] and a not in s:
-        r=a
-print(r)
-```
-#### java
-```java
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -67,4 +42,3 @@ public class p28432 {
         System.out.println(r);
     }
 }
-```
