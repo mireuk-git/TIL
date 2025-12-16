b1988dfd3efa5ac20b8aa84a9995c8deaf4e495f
mireuk-git <alexyunwoo@gmail.com>
2025-12-16 09:18:20 +0900

    p25305 2025.12.15

--- java/codekata/src/p25305.java
+++ java/codekata/src/p25305.java
@@ -1,87 +1,4 @@
-# Codekata
-### 32864
-[link](https://www.acmicpc.net/problem/32684)
-#### python
-```python
-p1 = list(map(int,input().split()))
-p2 = list(map(int,input().split()))
-
-p1_score = 13*p1[0]+7*p1[1]+5*p1[2]+3*p1[3]+3*p1[4]+2*p1[5]
-p2_score = 13*p2[0]+7*p2[1]+5*p2[2]+3*p2[3]+3*p2[4]+2*p2[5] + 1.5
-if p1_score > p2_score: print("cocjr0208")
-else: print("ekwoo")
-```
-
-#### java
-```java
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class p32684 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] p1 = new int[6];
-        for (int i=0;i<6;i++) p1[i] = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] p2 = new int[6];
-        for (int i=0;i<6;i++) p2[i] = Integer.parseInt(st.nextToken());
-        double p1Score = p1[0]*13 + p1[1]*7 + p1[2]*5 + p1[3]*3 + p1[4]*3 + p1[5]*2;
-        double p2Score = p2[0]*13 + p2[1]*7 + p2[2]*5 + p2[3]*3 + p2[4]*3 + p2[5]*2 + 1.5;
-        if (p1Score > p2Score) System.out.println("cocjr0208");
-        else System.out.println("ekwoo");
-    }
-}
-```
-
-### 25305
-[link](https://www.acmicpc.net/problem/25305)
-#### python
-<b>1st try:</b>
-
-```python
-n,k = map(int,input().split())
-x = list(map(int,input().split()))
-x.sort(reverse=True)
-print(x[k-1])
-```
-
-<b>2nd try:</b>
-
-```python
-def heapify(arr, index, heapsize):
-    largest = index
-    left = 2*index+1
-    right = 2*index+2
-    if left < heapsize and arr[left] > arr[largest]: largest = left
-    if right < heapsize and arr[right] > arr[largest]: largest = right
-    if largest != index:
-        arr[largest],arr[index] = arr[index],arr[largest]
-        heapify(arr,largest,heapsize)
-
-def heapsort(arr):
-    n = len(arr)
-    for i in range(n//2-1, -1, -1):
-        heapify(arr,i,n)
-    for i in range(n-1,0,-1):
-        arr[0],arr[i] = arr[i],arr[0]
-        heapify(arr,0,i)
-    return arr
-
-n,k = map(int,input().split())
-x = list(map(int,input().split()))
-heapsort(x)
-print(x[-k])
-```
-- heapsort를 구현해서 정렬
-
-#### java
-
-<b>1st try:</b>
-
-```java
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -103,11 +20,8 @@ public class p25305 {
         System.out.println(x.get(k-1));
     }
 }
-```
+*/
 
-<b>2nd try:</b>
-
-```java
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -155,4 +69,3 @@ public class p25305{
         System.out.println("\n"+x[n-k]);
     }
 }
-```
