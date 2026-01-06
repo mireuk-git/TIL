1f3f799db9f0e828370f5e5de676e7848d40a1be
mireuk-git <alexyunwoo@gmail.com>
2026-01-06 09:53:25 +0900

    2026.01.06

---
+++ 2026_01_06.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/11121
+# 11121
+'''
+t=int(input())
+for i in range(t):
+    feed,out = input().split()
+    if feed==out:
+        print("OK")
+    else: print("ERROR")
+'''
+# https://www.acmicpc.net/problem/1834
+# 1834
+'''
+n=int(input())
+sum=0
+for i in range(1,n):
+    sum+=n*i+i
+print(sum)
+'''
