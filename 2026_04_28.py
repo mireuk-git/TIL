5ee81ffad137e271dad41a5d118b8bc67d613bac
mireuk-git <alexyunwoo@gmail.com>
2026-04-28 16:46:55 +0900

    2026.04.28

---
+++ 2026_04_28.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/28279
+
+from collections import deque
+
+n=int(input())
+d=deque()
+for i in range(n):
+    command=list(map(int,input().split()))
+    if command[0]==1:
+        d.appendleft(command[1])
+    elif command[0]==2:
+        d.append(command[1])
+    elif command[0]==3:
+        if len(d)!=0: print(d.popleft())
+        else: print(-1)
+    elif command[0]==4:
+        if len(d)!=0: print(d.pop())
+        else: print(-1)
+    elif command[0]==5:
+        print(len(d))
+    elif command[0]==6:
+        if len(d)==0: print(1)
+        else: print(0)
+    elif command[0]==7:
+        if len(d)==0: print(-1)
+        else: print(d[0])
+    elif command[0]==8:
+        if len(d)==0: print(-1)
+        else: print(d[-1])
