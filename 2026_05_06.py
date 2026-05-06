164a234a30dd4bfa01d179e0d33adfc335092491
mireuk-git <alexyunwoo@gmail.com>
2026-05-06 17:00:19 +0900

    2026.05.06

--- 2026_05_06.py
+++ 2026_05_06.py
@@ -1,5 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/176963
-
+'''
 def solution(name, yearning, photo):
     result = [0]*len(photo)
     for i in range(len(photo)):
@@ -13,3 +13,42 @@ name=["may", "kein", "kain", "radi"]
 yearning=[5, 10, 1, 3]
 photo=[["may"],["kein", "deny", "may"], ["kon", "coni"]]
 print(solution(name,yearning,photo))
+'''
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/161990
+
+def solution(wallpaper):
+    result=[]
+
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j]=='#':
+                result.append(i)
+                break
+        if len(result)==1: break
+
+    for i in range(len(wallpaper[0])):
+        for j in range(len(wallpaper)):
+            if wallpaper[j][i]=='#':
+                result.append(i)
+                break
+        if len(result)==2: break
+
+    for i in range(len(wallpaper)-1,-1,-1):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j]=='#':
+                result.append(i+1)
+                break
+        if len(result)==3: break
+
+    for i in range(len(wallpaper[-1])-1,-1,-1):
+        for j in range(len(wallpaper)):
+            if wallpaper[j][i]=='#':
+                result.append(i+1)
+                break
+        if len(result)==4: break
+
+    return result
+
+wallpaper = [".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."]
+print(solution(wallpaper))
