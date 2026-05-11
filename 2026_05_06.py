3e35e7a622065bebc7340dca6830c0f65a74c1e4
mireuk-git <alexyunwoo@gmail.com>
2026-05-11 12:11:07 +0900

    2026.05.06

--- 2026_05_06.py
+++ 2026_05_06.py
@@ -1,19 +1,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/176963
-'''
+
 def solution(name, yearning, photo):
     result = [0]*len(photo)
+    d = dict(zip(name,yearning))
     for i in range(len(photo)):
-        for j in range(len(photo[i])):
-            for k in range(len(name)):
-                if photo[i][j]==name[k]:
-                    result[i]+=yearning[k]
+        for j in photo[i]:
+            if i in name:
+                result[i]+=d[j]
     return result
 
 name=["may", "kein", "kain", "radi"]
 yearning=[5, 10, 1, 3]
 photo=[["may"],["kein", "deny", "may"], ["kon", "coni"]]
 print(solution(name,yearning,photo))
-'''
+
 
 # https://school.programmers.co.kr/learn/courses/30/lessons/161990
 
